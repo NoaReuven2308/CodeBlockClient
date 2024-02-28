30be254839fedf1bc2107f241f669a4afc5ecc6e
NoaReuven2308 <161506474+NoaReuven2308@users.noreply.github.com>
2024-02-28 10:08:07 +0200

    Update Lobby.js

--- src/Lobby.js
+++ src/Lobby.js
@@ -3,39 +3,51 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './styles.css';
 
+/**
+ * Lobby Component:
+ * This component represents the main page where users can view and interact with code blocks.
+ * Users can choose existing code blocks or add new ones.
+ */
 function Lobby() {
-  const [codeBlocks, setCodeBlocks] = useState([]);
-  const [showForm, setShowForm] = useState(false);
-  const [newTitle, setNewTitle] = useState('');
-  const [newCode, setNewCode] = useState('');
+  // State variables to manage code blocks and form visibility
+  const [codeBlocks, setCodeBlocks] = useState([]); // Stores the list of code blocks
+  const [showForm, setShowForm] = useState(false); // Controls the visibility of the form
+  const [newTitle, setNewTitle] = useState(''); // Stores the title of the new code block being added
+  const [newCode, setNewCode] = useState(''); // Stores the code of the new code block being added
+
+  // Hook from React Router for navigation
   const navigate = useNavigate();
 
+  // Fetch code blocks from the server when the component mounts
   useEffect(() => {
     axios.get('https://codeblockserver-production.up.railway.app/api/codeblocks')
       .then(response => {
-        setCodeBlocks(response.data);
+        setCodeBlocks(response.data); // Update codeBlocks state with fetched data
       })
       .catch(error => {
         console.error('There was an error fetching the code blocks:', error);
       });
   }, []);
 
+  // Function to navigate to a selected code block
   const selectCodeBlock = (id, title) => {
     navigate(`/codeblock/${id}`, { state: { title } });
   };
 
+  // Toggle the visibility of the form for adding new code blocks
   const handleAddNew = () => {
     setShowForm(!showForm);
   };
 
+  // Handle form submission when adding a new code block
   const handleSubmit = (e) => {
     e.preventDefault();
     if (newTitle && newCode) {
       axios.post('https://codeblockserver-production.up.railway.app/api/codeblocks', { title: newTitle, code: newCode })
         .then(response => {
           console.log('New CodeBlock added:', response.data);
-          setCodeBlocks([...codeBlocks, response.data]);
-          setShowForm(false);
+          setCodeBlocks([...codeBlocks, response.data]); // Update codeBlocks state with the new code block
+          setShowForm(false); // Hide the form after successful addition
         })
         .catch(error => {
           console.error('Error adding new CodeBlock:', error);
@@ -43,9 +55,10 @@ function Lobby() {
     }
   };
 
+  // Cancel adding a new code block and reset form fields
   const handleCancel = () => {
     setShowForm(false);
-    // You can also clear the form fields here if needed
+    // Reset form fields
     setNewTitle('');
     setNewCode('');
   };
