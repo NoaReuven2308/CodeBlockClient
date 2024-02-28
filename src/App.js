a94f632d03e0f7b1f569602ea23f0168d3805658
NoaReuven2308 <161506474+NoaReuven2308@users.noreply.github.com>
2024-02-28 10:32:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import io from 'socket.io-client';
+import Lobby from './Lobby';
+import CodeBlockPage from './CodeBlockPage';
+
+// Initialize socket connection
+const socket = io('https://codeblockserver-production.up.railway.app/'); 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/lobby" element={<Lobby />} />
+          <Route path="/codeblock/:id" element={<CodeBlockPage socket={socket} mentor={true} />} />
+        </Routes>
+    </Router>
   );
 }
 
